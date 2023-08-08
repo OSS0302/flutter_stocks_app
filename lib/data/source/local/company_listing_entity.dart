9a411363943992a1e4a72dea17d34136f7cf67d0
ohsangsoo <tkdtn3264@gmail.com>
2023-08-08 18:03:49 +0900

    각각의 객체에 하이브 필드 각각 선언하고 object 상속 받으면Entity 설계가 끝이났다.

---
+++ lib/data/source/local/company_listing_entity.dart
@@ -0,0 +1,22 @@
+import 'package:hive_flutter/adapters.dart';
+
+@HiveType(typeId: 0)
+class CompanyListingEntity extends HiveObject{
+  @HiveField(0)
+  String symbol;
+  @HiveField(1)
+  String name;
+  @HiveField(2)
+  String exchange;
+  @HiveField(3)
+  String ipoDate;
+
+  CompanyListingEntity({ // 생성자 추가
+   required this.symbol,
+   required this.name,
+   required this.exchange,
+   required this.ipoDate,
+  });
+
+
+}
