9812ebf370cfb4ce10ee31a3e84598bad065c7a7
ohsangsoo <tkdtn3264@gmail.com>
2023-08-09 19:57:17 +0900

    매번 api key 를 사용하기 귀찮아서 변경

--- lib/data/source/remote/stock_api.dart
+++ lib/data/source/remote/stock_api.dart
@@ -8,7 +8,7 @@ class StockApi {
   final http.Client client; //통신하기 위해서 클라이언트 정의해야한다
   StockApi(this.client); // 생성자 추가
 
-  Future<http.Response> getListings(String apiKey) async {
+  Future<http.Response> getListings({String apiKey = apiKey}) async {
     // Response 객체 로 이제 데이터가 들어간다.
        return await client.get(Uri.parse('https://www.alphavantage.'
         'co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey=$apiKey'));
