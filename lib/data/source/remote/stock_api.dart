8b5d5ff7b186576bb798aad39b3991ab4a77ba14
ohsangsoo <tkdtn3264@gmail.com>
2023-08-09 20:56:31 +0900

    옵션 추가

--- lib/data/source/remote/stock_api.dart
+++ lib/data/source/remote/stock_api.dart
@@ -6,11 +6,10 @@ class StockApi {
   static const apiKey = 'U80NMMGLBQOFOAJL'; // 키 넣기
 
   final http.Client _client; //통신하기 위해서 클라이언트 정의해야한다
-  StockApi(http.Client? client):_client=(client?? http.Client()) ; // 생성자 추가
+  StockApi({http.Client? client}): _client=(client?? http.Client()); // 생성자 추가
 
   Future<http.Response> getListings({String apiKey = apiKey}) async {
     // Response 객체 로 이제 데이터가 들어간다.
-       return await _client.get(Uri.parse('https://www.alphavantage.'
-        'co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey=$apiKey'));
+       return await _client.get(Uri.parse('https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=$apiKey'));
   }
 }
