2f0b23346f0b94cd8d6b311fbc7ea403a46916ee
ohsangsoo <tkdtn3264@gmail.com>
2023-08-11 16:34:40 +0900

    Mutiprovider main에서 로직 추가하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_stocks_app/data/source/local/company_listing_entity.dart';
+import 'package:flutter_stocks_app/data/source/local/stock_dao.dart';
+import 'package:flutter_stocks_app/data/source/remote/stock_api.dart';
+import 'package:flutter_stocks_app/data/source/stock_repository_impl.dart';
+import 'package:flutter_stocks_app/presentation/company_listings/company_listings_screen.dart';
+import 'package:flutter_stocks_app/presentation/company_listings/company_listings_view_model.dart';
 import 'package:flutter_stocks_app/util/color_schemes.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 
-void main() async{
+void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(CompanyListingEntityAdapter());
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => CompanyListingsViewModel(
+            StockRepositoryImpl(
+              StockApi(),
+              StockDao(),
+            ),
+          ),
+        )
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +35,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       // 밝은 모드
       theme: ThemeData(
         useMaterial3: true,
@@ -27,9 +46,9 @@ class MyApp extends StatelessWidget {
         colorScheme: darkColorScheme,
       ),
       themeMode: ThemeMode.system,
+
       // 시스템의 테마를 따라간다. 본인의 스마트폰이 타크모드이면 타크모드가된다.
-      home: CompanyList
+      home: const CompanyListingsScreen(),
     );
   }
 }
-
