ca63b08db950a915586d56213f2ba7e442de9408
ohsangsoo <tkdtn3264@gmail.com>
2023-08-12 17:38:58 +0900

    stock_repository.dart 에 두번째 info 넣기

--- lib/domain/repository/stock_repository.dart
+++ lib/domain/repository/stock_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_stocks_app/domain/model/company_info.dart';
 import 'package:flutter_stocks_app/domain/model/company_listing.dart';
 import 'package:flutter_stocks_app/util/result.dart';
 
@@ -6,4 +7,6 @@ abstract class StockRepository{
     bool fetchFromRemote,
     String query,
   );
-}
+  // symbol 주어지면  그거에대한CompanyInfo를 리턴한다..
+  Future<Result<CompanyInfo>> getCompanyInfo(String symbol);
+ }
