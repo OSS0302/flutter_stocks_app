1e9e6cc526e2ac8c7c5843623dedf6b27211d019
ohsangsoo <tkdtn3264@gmail.com>
2023-08-12 17:38:10 +0900

    두번째 화면 뷰모델

---
+++ lib/presentation/company_info/company_info_view_model.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stocks_app/domain/repository/stock_repository.dart';
+import 'package:flutter_stocks_app/presentation/company_info/company_info_state.dart';
+
+class  CompanyInfoViewModel with ChangeNotifier {
+  final StockRepository _repository;
+  var _state = CompanyInfoState();
+
+  CompanyInfoState get state => _state;
+
+  CompanyInfoViewModel(this._repository, String symbol){
+    loadCompanyInfo(symbol);
+  }
+
+  Future<void> loadCompanyInfo(String symbol) async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners(); // 이벤트 감지
+    // 실제 데이터 가져오기
+    final result = await _repository.getCompanyInfo(symbol);
+    result.when(
+      success: (info) {
+        _state = state.copyWith(
+          companyInfo: info,
+          isLoading: false,
+          errorMessage: null,
+        );
+      },
+      error: (e) {
+        _state = state.copyWith(
+          companyInfo: null,
+          isLoading: false,
+          errorMessage: e.toString(),
+        );
+      } ,
+    );
+    notifyListeners(); // 이벤트 감지
+  }
+}
