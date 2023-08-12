98e206863fe5618073947c51e80b9384e6639ca2
ohsangsoo <tkdtn3264@gmail.com>
2023-08-12 17:42:32 +0900

    Get it 로직 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_stocks_app/data/source/local/company_listing_entity.dart';
 import 'package:flutter_stocks_app/data/source/local/stock_dao.dart';
 import 'package:flutter_stocks_app/data/source/remote/stock_api.dart';
-import 'package:flutter_stocks_app/data/source/stock_repository_impl.dart';
+import 'package:flutter_stocks_app/data/repository/stock_repository_impl.dart';
+import 'package:flutter_stocks_app/domain/repository/stock_repository.dart';
 import 'package:flutter_stocks_app/presentation/company_listings/company_listings_screen.dart';
 import 'package:flutter_stocks_app/presentation/company_listings/company_listings_view_model.dart';
 import 'package:flutter_stocks_app/util/color_schemes.dart';
+import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(CompanyListingEntityAdapter());
+  final repository = StockRepositoryImpl(
+    StockApi(),
+    StockDao(),
+  );
+    // get it
+  GetIt.instance.registerSingleton<StockRepository> (repository);// 사용하면 어디서나 쉽게 쓸수있다
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
           create: (_) => CompanyListingsViewModel(
-            StockRepositoryImpl(
-              StockApi(),
-              StockDao(),
+              repository,
             ),
-          ),
-        )
+        ),
       ],
       child: const MyApp(),
     ),
