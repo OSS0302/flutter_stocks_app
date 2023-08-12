f1539cb2c131a6ed88c70a8eca784a068997bfa5
ohsangsoo <tkdtn3264@gmail.com>
2023-08-12 17:42:10 +0900

    검색 할때 500밀리초 동안 딜레이 불필요한 동작 하지 않도록 시간 로직 추가

--- lib/presentation/company_listings/company_listings_view_model.dart
+++ lib/presentation/company_listings/company_listings_view_model.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_stocks_app/domain/repository/stock_repository.dart';
+import 'package:flutter_stocks_app/presentation/company_listings/company_listings_action.dart';
 import 'package:flutter_stocks_app/presentation/company_listings/company_listings_state.dart';
 
+import 'dart:async';
 class CompanyListingsViewModel with ChangeNotifier {
   final StockRepository _repository;
-  var _state = CompanyListingsState(); //디폴트 값을 넣어줘서 아무것도 세팅을 안해도되며
+  var _state =   CompanyListingsState(); //디폴트 값을 넣어줘서 아무것도 세팅을 안해도되며
+
+  Timer? _debounce; //불필요한 동작 을 줄이기위해서 시간을 준다.
 
   CompanyListingsState get state => _state; //  getter 를 통해서 외부에서 조작할 수있도록 한다.
 
@@ -12,6 +16,20 @@ class CompanyListingsViewModel with ChangeNotifier {
     // 이부분에 유스 케이스 가 들어가도 되지만 이 프로젝트에서 기능이 적어서 레포지토리를 가져온다.
     // 데이터를 로드 하겠따.그래서 _state를 채우겠다.
     _getCompanyListing();
+  }
+
+  void onAction(CompanyListingsAction action) {
+    // 새로고침과 검색 두가지 기능을 사용자가 하게된다.
+    action.when(
+      refresh: ()=> _getCompanyListing(fetchFromRemote: true),// 리모트 해서 강제 콜해서 리스트를 불러온다.
+      onSearchQueryChange: (query) {
+        _debounce?.cancel();
+        _debounce= Timer(const Duration (milliseconds: 500), (){
+          // db에서 동작한다.
+          _getCompanyListing(query: query); // 위애 쿼리를 가져와서   _getCompanyListing(query: query)로 들어간다.
+        });
+      },
+    );
   }
 
   Future _getCompanyListing({
@@ -27,14 +45,15 @@ class CompanyListingsViewModel with ChangeNotifier {
     // 실제 데이터 가져오는 로직
     final reuslt = await _repository.getCompanyListings(fetchFromRemote, query);
     reuslt.when(
-      success: (listings) { // 성공시 내용물 변경
+      success: (listings) {
+        // 성공시 내용물 변경
         _state = state.copyWith(
           companies: listings,
         );
       },
       error: (e) {
         //TODO: 에러처리
-        print('리모트 에러'+e.toString());
+         print('리모트 에러 $e');
       },
     );
 
