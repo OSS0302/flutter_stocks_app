266e6a5f5a2b19d284d9b11c37e1428db16b08ac
ohsangsoo <tkdtn3264@gmail.com>
2023-08-13 17:25:10 +0900

    주식 정보 와 시간 repository 추가

--- lib/domain/repository/stock_repository.dart
+++ lib/domain/repository/stock_repository.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_stocks_app/domain/model/company_info.dart';
 import 'package:flutter_stocks_app/domain/model/company_listing.dart';
+import 'package:flutter_stocks_app/domain/model/intraday_info.dart';
 import 'package:flutter_stocks_app/util/result.dart';
 
 abstract class StockRepository{
@@ -9,4 +10,6 @@ abstract class StockRepository{
   );
   // symbol 주어지면  그거에대한CompanyInfo를 리턴한다..
   Future<Result<CompanyInfo>> getCompanyInfo(String symbol);
+  // 하루 주식 정보 추가
+  Future<Result<List<IntradayInfo>>>getIntradayInfo(String symbol);
  }
