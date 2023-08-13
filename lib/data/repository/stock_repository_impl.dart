651ee56d24d466e4d87093ce77cb6841189ce0cc
ohsangsoo <tkdtn3264@gmail.com>
2023-08-13 17:03:40 +0900

    하루 주식 정보 파서 stock_repository_impl.dart 에 추가하기

--- lib/data/repository/stock_repository_impl.dart
+++ lib/data/repository/stock_repository_impl.dart
@@ -1,16 +1,19 @@
 import 'package:flutter_stocks_app/data/csv/company_listings_parser.dart';
+import 'package:flutter_stocks_app/data/csv/intraday_info_parser.dart';
 import 'package:flutter_stocks_app/data/mapper/company_mapper.dart';
 import 'package:flutter_stocks_app/data/source/local/stock_dao.dart';
 import 'package:flutter_stocks_app/data/source/remote/stock_api.dart';
 import 'package:flutter_stocks_app/domain/model/company_info.dart';
 import 'package:flutter_stocks_app/domain/model/company_listing.dart';
+import 'package:flutter_stocks_app/domain/model/intraday_info.dart';
 import 'package:flutter_stocks_app/domain/repository/stock_repository.dart';
 import 'package:flutter_stocks_app/util/result.dart';
 
 class StockRepositoryImpl implements StockRepository{
   final StockApi _api;
   final StockDao _dao;
-  final _parser = CompanyListingsParser();
+  final _companyListingsParser = CompanyListingsParser(); // 회사 리스트 파서
+  final _intradayInfoParser = IntradayInfoParser(); // 하루 주식 정보 파서
 
   StockRepositoryImpl(this._api, this._dao);
 
@@ -32,7 +35,7 @@ class StockRepositoryImpl implements StockRepository{
   // 리모트 인  경우에는 에러 에 빠질 수있으므로
   try {
     final response = await _api.getListings();
-    final remoteListings = await _parser.parse(response.body);
+    final remoteListings = await _companyListingsParser.parse(response.body);
     // 캐시를 비우기
     await _dao.clearCompanyListing();
 
@@ -55,5 +58,17 @@ class StockRepositoryImpl implements StockRepository{
     }catch(e){
       return Result.error(Exception('회사 정보 로드 실패 !:${e.toString()}'));
     }
+  }
+
+  @override
+  Future<Result<List<IntradayInfo>>> getIntradayInfo(String symbol) async {
+   try{
+     final response = await _api.getIntradayInfo(symbol: symbol);// symbol를 전달 하면 response 가 응답 객체로 온다.
+     final results = await  _intradayInfoParser.parse(response.body);  // 파싱이되서 모든 데이터가 들어간다.
+     return Result.success(results);
+   }catch (e ){
+      return Result.error(Exception('intraday 정보 로드 실패!! ${e.toString()}')); // 실패시 에러메시지 보여준다.
+   }
+
   }
 }
